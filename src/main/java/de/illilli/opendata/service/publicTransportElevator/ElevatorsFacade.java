1bb4da2f178ba413ee5386010e6e6c56206cb67e
Wolfram Eberius <eberius@gmail.com>
2019-01-14 06:19:06 +0100

    Implement Converter for DataConversion #2

--- src/main/java/de/illilli/opendata/service/publicTransportElevator/ElevatorsFacade.java
+++ src/main/java/de/illilli/opendata/service/publicTransportElevator/ElevatorsFacade.java
@@ -1,30 +1,20 @@
 package de.illilli.opendata.service.publicTransportElevator;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import org.geojson.Feature;
-import org.geojson.FeatureCollection;
-import org.geojson.Point;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 
-import de.illilli.opendata.service.AskFor;
+import de.illilli.opendata.service.Converter;
 import de.illilli.opendata.service.Facade;
-import de.illilli.opendata.service.publicTransportElevator.askfor.AskForElevators;
 import de.illilli.opendata.service.publicTransportElevator.model.Elevator;
-import de.illilli.opendata.service.publicTransportElevator.model.Geometrie;
 
 public class ElevatorsFacade implements Facade {
 
 	List<Elevator> data = new ArrayList<Elevator>();
+	Converter<List<Elevator>, URL, String> converter = new ElevatorConverter();
 
 	public ElevatorsFacade() throws IOException {
 		this(ElevatorsFacade.class.getClassLoader().getResource("fahrtreppen.json"));
@@ -32,41 +22,13 @@ public class ElevatorsFacade implements Facade {
 
 	public ElevatorsFacade(URL url) throws IOException {
 
-		AskFor<InputStream> askFor = new AskForElevators(url);
-
-		FeatureCollection featureCollection = new ObjectMapper()
-				.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-				.readValue(askFor.getData(), FeatureCollection.class);
-
-		List<Feature> features = featureCollection.getFeatures();
-		for (Feature feature : features) {
-
-			Elevator elevator = new Elevator();
-
-			Map<String, Object> properties = feature.getProperties();
-			elevator.setId((String) properties.get("Kennung"));
-			elevator.setBezeichnung((String) properties.get("Bezeichnung"));
-			int haltestellenbereich = Integer.parseInt((String) properties.get("Haltestellenbereich"));
-			elevator.setHaltestellenbereich(haltestellenbereich);
-			elevator.setInfo((String) properties.get("Info"));
-
-			if (feature.getGeometry() instanceof Point) {
-				Point point = (Point) feature.getGeometry();
-				Geometrie geometrie = new Geometrie();
-				geometrie.setType("Point");
-				geometrie.setLat(point.getCoordinates().getLatitude());
-				geometrie.setLng(point.getCoordinates().getLongitude());
-				elevator.setGeometrie(geometrie);
-			}
-
-			data.add(elevator);
-		}
+		this.data = converter.getAsObject(url);
 
 	}
 
 	@Override
 	public String getJson() throws JsonProcessingException {
-		return new Gson().toJson(data);
+		return converter.getAsString(data);
 	}
 
 }
